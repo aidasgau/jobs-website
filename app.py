2ffeb381839f5ce360ab91a020014663899cd564
Aidas Gaučas <aidasgau2@gmail.com>
2023-08-28 15:27:47 +0300

    Added bootstrap styles, updated HTML and some other styles

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template('home.html')
+def index():
+    full_name = 'Aidas Gaučas'
+    email = 'aidasgau2@gmail.com'
+    github = 'https://github.com/aidasgau'
+    current_year = datetime.datetime.now().year
+    return render_template('home.html', current_year=current_year, full_name=full_name, email=email, github=github)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
